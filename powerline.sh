2ac069c8a735a021858851773d143abd4535f34b
Alex Shin <alexdowell91@gmail.com>
2020-02-17 13:28:07 +0300

    powerline edit no idea this works or no

--- powerline.sh
+++ powerline.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# supa minimal powerline installation for zsh, works only for dnf package manager
 sudo dnf install -y zsh powerline-fonts
+sudo chsh -s $(which zsh) $USER
+
+# installing Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/shinzxc/dots
-cp dots/.zshrc  ~/.zshrc	
+
+cp dots/.zshrc  ~/.zshrc
+
+echo "cleaning shit"
+rm -rf dots/
+
 echo "powerline installed"
